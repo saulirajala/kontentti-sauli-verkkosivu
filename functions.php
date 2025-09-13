26d8f2755c90f7cd82b640d042f93851566956a9
Sauli Rajala <sauli.rajala@valu.fi>
2025-09-13 15:44:08 +0300

    Update

--- functions.php
+++ functions.php
@@ -1,1 +1,22 @@
 <?php
+
+add_filter( 'wp_theme_json_data_theme', function( $theme_json ) {
+    $custom_css_file = get_stylesheet_directory() . '/assets/css/custom-styles.css';
+
+    if ( file_exists( $custom_css_file ) ) {
+        $custom_css = file_get_contents( $custom_css_file );
+
+        if ( ! empty( $custom_css ) ) {
+             $new_data = [
+                'version'  => 3, 
+                'styles'   => [
+                    'css' => $custom_css
+                ],
+            ];
+
+            return $theme_json->update_with( $new_data );
+        }
+    }
+
+    return $theme_json;
+});
