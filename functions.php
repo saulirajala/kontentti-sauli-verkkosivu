8427f79e9a4e86d5905529cb89a9ab35e3d4fd40
Sauli Rajala <sauli.rajala@valu.fi>
2025-09-13 20:20:59 +0300

    Test

--- functions.php
+++ functions.php
@@ -42,3 +42,85 @@ add_filter('show_admin_bar', function () {
 
     return false;
 });
+
+
+/**
+ * Lue postcontent.txt, parsii lohkot ja päivitä kuvien osoitteet.
+ */
+function process_postcontent_with_local_images() {
+    // $file = get_stylesheet_directory() . '/playground/postcontent.txt';
+
+    // if ( ! file_exists( $file ) ) {
+    //     // return new WP_Error( 'file_missing', 'postcontent.txt ei löytynyt teemakansiosta.' );
+    // }
+
+    // $content = file_get_contents( $file );
+
+    // if ( empty( $content ) ) {
+    //     // return new WP_Error( 'file_empty', 'postcontent.txt on tyhjä.' );
+    // }
+
+    // // Parsitaan sisällöstä lohkot
+    // $blocks = parse_blocks( $content );
+
+    // // Käydään lohkot rekursiivisesti läpi
+    // $blocks = update_image_blocks( $blocks );
+
+    // // Palautetaan uusi sisältö lohkoista
+    // $updated_content = serialize_blocks( $blocks );
+
+    // return $updated_content;
+}
+
+/**
+ * Käy lohkot läpi rekursiivisesti ja päivittää core/image -lohkot.
+ */
+function update_image_blocks( $blocks ) {
+    foreach ( $blocks as &$block ) {
+        // Jos on kuva
+        if ( $block['blockName'] === 'core/image' && ! empty( $block['attrs']['url'] ) ) {
+            $old_url = $block['attrs']['url'];
+            $filename = basename( parse_url( $old_url, PHP_URL_PATH ) );
+
+            // Hae attachment tiedostonimen perusteella
+            $attachment_id = attachment_url_to_postid( wp_get_upload_dir()['baseurl'] . '/' . $filename );
+
+            if ( ! $attachment_id ) {
+                // Jos ei löytynyt, kokeillaan etsiä mediatiedostoa suoraan nimen perusteella
+                $attachment_id = mytheme_find_attachment_by_filename( $filename );
+            }
+
+            if ( $attachment_id ) {
+                $new_url = wp_get_attachment_url( $attachment_id );
+                if ( $new_url ) {
+                    $block['attrs']['url'] = $new_url;
+                }
+            }
+        }
+
+        // Jos on sisäkkäisiä lohkoja → käsitellään nekin
+        if ( ! empty( $block['innerBlocks'] ) ) {
+            $block['innerBlocks'] = update_image_blocks( $block['innerBlocks'] );
+        }
+    }
+
+    return $blocks;
+}
+
+/**
+ * Etsi attachment filename perusteella (jos attachment_url_to_postid ei riitä).
+ */
+function mytheme_find_attachment_by_filename( $filename ) {
+    global $wpdb;
+
+    $query = $wpdb->prepare(
+        "SELECT ID FROM {$wpdb->posts}
+        WHERE post_type = 'attachment'
+        AND post_status = 'inherit'
+        AND guid LIKE %s
+        LIMIT 1",
+        '%' . $wpdb->esc_like( $filename )
+    );
+
+    return $wpdb->get_var( $query );
+}
